80c4585ae24eaf7a8a835761ebde88527076a464
tusharkumar <tusharkumar27864@gmail.com>
2024-02-21 13:58:39 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const heading0 = React.createElement("h1" , {} , "Hello World!");
+
+const heading1 = React.createElement("h1" , {} , "heading 1");
+const heading2 = React.createElement("h1" , {} , "heading 2");
+
+
+const container = React.createElement("div" , {} , [heading0 , heading1 , heading2]);
+  
+
+root.render(container);
+
+
+ 
