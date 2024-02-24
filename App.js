67585f73689ccc9cde0ed6d8856e8fa250883397
tusharkumar <tusharkumar27864@gmail.com>
2024-02-24 19:14:51 +0530

    made some changes

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ import  ReactDOM  from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const heading0 = React.createElement("h1" , {} , "Hello World!");
-const heading1 = React.createElement("h1" , {} , "heading 1");
-const heading2 = React.createElement("h1" , {} , "heading 2");
+const heading0 = React.createElement("h1" , {key : "h0"} , "Hello World!");
+const heading1 = React.createElement("h1" , {key : "h1"} , "heading 1");
+const heading2 = React.createElement("h1" , {key : "h2"} , "heading 2");
 const container = React.createElement("div" , {} , [heading0 , heading1 , heading2]);
   
 root.render(container); 
